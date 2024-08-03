f207eaf19c9b8d22f5dd8cf2e476370fd20f41ef
ShoeChicken0 <shoechicken0@gmail.com>
2024-08-03 20:40:36 +1000

    Using wodle wordlist

--- wordle.py
+++ wordle.py
@@ -1,30 +1,13 @@
 import re
-import nltk
-from nltk.corpus import words
 import random
-import enchant
+import configparser
 
-nltk.download('words')
-dictionary = enchant.Dict("en_US")
-# Get the list of words
-word_list = words.words()
-pattern = re.compile(r'^[a-z]{5}$')
-five_letter_words = [word for word in word_list if pattern.match(word)]
-print(f"nltk word list: {len(five_letter_words)}")
-matching_words = [word for word in five_letter_words if dictionary.check(word)]
-dictionary_size = len(matching_words)
-print(f"valid word list: {dictionary_size}")
-random_index = random.randint(0, dictionary_size - 1)
-guess = matching_words[random_index]
-print(guess)
+# Function to read words from a text file into a list
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        words = [line.strip() for line in file.readlines()]
+    return words
 
-guess_param = {
-    "guess": guess,
-    "excluded_letters": "",
-    "correct_letters": [],
-    "wrong_position" : ["", "", "", "", ""],
-    "correct_positions": ["0","0","0","0","0"]
-}
 
 # regex for letters that must be included
 def correct_expression(guess_param):
@@ -51,34 +34,18 @@ def generate_list(guess_param, word_list):
                 exclude_expression = exclude_letters + guess_param["wrong_position"][index]
             else:
                 exclude_expression = exclude_letters
-            if counter2 > 0:
-                for i in range(counter2):
-                    correct_letters += guess_param["correct_positions"][position2+i]
-                search_expression += f"{correct_letters}"
-                correct_letters = ""
-                counter2 = 0
             search_expression += f"[^{exclude_expression}]{{1}}"
         else:
-            if index == 0:
-                counter2 += 1
-                position2 = index
-            else:
-                if counter2 == 0:
-                    position2 = index
-                counter2 += 1
-            if index == 4:
-                for i in range(counter2):
-                    correct_letters += guess_param["correct_positions"][position2+i]
-                search_expression += f"{correct_letters}"
+            search_expression += guess_param["correct_positions"][index]
     search_expression += "$"
 
-    #search_expression = f"{include_letters}^[^{guess_param['excluded_letters']}]{{5}}$"
     print(search_expression)
     pattern = re.compile(search_expression)
     matching_words = [word for word in word_list if pattern.match(word)]
 
     print(len(matching_words))
     return matching_words
+
 
 # update the guess parameters based on feedback
 def update_parameters(guess_param, feedback):
@@ -98,11 +65,28 @@ def update_parameters(guess_param, feedback):
 
         else:
             print("Wrong letter")
-            if letter not in guess_param["excluded_letters"]:
+            if letter not in guess_param["excluded_letters"] and letter not in guess_param["correct_letters"]:
                 guess_param["excluded_letters"] += letter
-
     print(guess_param)
 
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+file_path = config['DEFAULT']['LIST_FILE_PATH']
+word_list = read_file(file_path)
+dictionary_size = len(word_list)
+print(f"valid word list: {dictionary_size}")
+random_index = random.randint(0, dictionary_size - 1)
+guess = word_list[random_index]
+print(guess)
+
+guess_param = {
+    "guess": guess,
+    "excluded_letters": "",
+    "correct_letters": [],
+    "wrong_position" : ["", "", "", "", ""],
+    "correct_positions": ["0","0","0","0","0"]
+}
 
 feedback = 00000
 while feedback != 22222:
@@ -111,9 +95,9 @@ while feedback != 22222:
     if feedback == "22222":
         break
     update_parameters(guess_param, feedback)
-    matching_words = generate_list(guess_param, matching_words)
-    random_index = random.randint(0, len(matching_words) - 1)
-    guess = matching_words[random_index]
+    word_list = generate_list(guess_param, word_list)
+    random_index = random.randint(0, len(word_list) - 1)
+    guess = word_list[random_index]
     guess_param["guess"] = guess
     print(guess)
 print("Game Won!")
