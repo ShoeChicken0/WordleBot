4d07c1fbbb19140031c72d7f6a0d491596588376
ShoeChicken0 <shoechicken0@gmail.com>
2024-08-03 17:17:13 +1000

    working regex no repeated letters list

--- wordle.py
+++ wordle.py
@@ -2,33 +2,27 @@ import re
 import nltk
 from nltk.corpus import words
 import random
-
+import enchant
 
 nltk.download('words')
-
+dictionary = enchant.Dict("en_US")
 # Get the list of words
 word_list = words.words()
-
-# Define a regex pattern
-pattern = re.compile(r'^[a-z]{5}$')  # Example: words with exactly 5 lowercase letters
-
-# Search for words matching the pattern
-matching_words = [word for word in word_list if pattern.match(word)]
-
-# Print the matching words
-print(len(matching_words))
-
+pattern = re.compile(r'^(?!.*(.).*\1)[a-z]{5}$')
+five_letter_words = [word for word in word_list if pattern.match(word)]
+print(f"nltk word list: {len(five_letter_words)}")
+matching_words = [word for word in five_letter_words if dictionary.check(word)]
 dictionary_size = len(matching_words)
+print(f"valid word list: {dictionary_size}")
 random_index = random.randint(0, dictionary_size - 1)
 guess = matching_words[random_index]
-
 print(guess)
 
 guess_param = {
     "guess": guess,
     "excluded_letters": "",
     "correct_letters": [],
-    "correct_positions": [0,0,0,0,0]
+    "correct_positions": ["0","0","0","0","0"]
 }
 
 # regex for letters that must be included
@@ -42,7 +36,45 @@ def correct_expression(guess_param):
 # filter the list of words based on regex pattern from guess param
 def generate_list(guess_param, word_list):
     include_letters = correct_expression(guess_param)
-    search_expression = f"{include_letters}^[^{guess_param['excluded_letters']}]{{5}}$"
+    exclude_letters = f"[^{guess_param["excluded_letters"]}]"
+    correct_letters = ""
+    search_expression = ""
+    # counters for consecutive letter inclusion/exclusion (counter1) and correct position (counter2)
+    counter1 = 0
+    counter2 = 0
+    position2 = 0
+
+    for index, position in enumerate(guess_param["correct_positions"]):
+        if position == "0":
+            if index == 0:
+                counter1 += 1
+
+            else:
+                counter1 += 1
+                if counter2 > 0:
+                    for i in range(counter2):
+                        correct_letters += guess_param["correct_positions"][position2+i]
+                    search_expression += f"{correct_letters}"
+                    counter2 = 0
+            if index == 4:
+                search_expression += f"{include_letters}{exclude_letters}{{{counter1}}}$"
+        else:
+            if index == 0:
+                counter2 += 1
+                position2 = index
+            else:
+                if counter2 == 0:
+                    position2 = index
+                counter2 += 1
+                if counter1 > 0:
+                    search_expression += f"{include_letters}{exclude_letters}{{{counter1}}}"
+                    counter1 = 0
+            if index == 4:
+                for i in range(counter2):
+                    correct_letters += guess_param["correct_positions"][position2+i]
+                    search_expression += f"{correct_letters}$"
+
+    #search_expression = f"{include_letters}^[^{guess_param['excluded_letters']}]{{5}}$"
     print(search_expression)
     pattern = re.compile(search_expression)
     matching_words = [word for word in word_list if pattern.match(word)]
