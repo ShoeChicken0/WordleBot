e4b926161c5897bedee524cfa1b17f12ad9ee3e4
ShoeChicken0 <shoechicken0@gmail.com>
2024-08-03 18:43:14 +1000

    working wordle guesser

--- wordle.py
+++ wordle.py
@@ -8,7 +8,7 @@ nltk.download('words')
 dictionary = enchant.Dict("en_US")
 # Get the list of words
 word_list = words.words()
-pattern = re.compile(r'^(?!.*(.).*\1)[a-z]{5}$')
+pattern = re.compile(r'^[a-z]{5}$')
 five_letter_words = [word for word in word_list if pattern.match(word)]
 print(f"nltk word list: {len(five_letter_words)}")
 matching_words = [word for word in five_letter_words if dictionary.check(word)]
@@ -22,6 +22,7 @@ guess_param = {
     "guess": guess,
     "excluded_letters": "",
     "correct_letters": [],
+    "wrong_position" : ["", "", "", "", ""],
     "correct_positions": ["0","0","0","0","0"]
 }
 
@@ -36,28 +37,26 @@ def correct_expression(guess_param):
 # filter the list of words based on regex pattern from guess param
 def generate_list(guess_param, word_list):
     include_letters = correct_expression(guess_param)
-    exclude_letters = f"[^{guess_param["excluded_letters"]}]"
+    exclude_letters = guess_param["excluded_letters"]
     correct_letters = ""
-    search_expression = ""
+    search_expression = f"{include_letters}"
     # counters for consecutive letter inclusion/exclusion (counter1) and correct position (counter2)
-    counter1 = 0
     counter2 = 0
     position2 = 0
 
     for index, position in enumerate(guess_param["correct_positions"]):
         if position == "0":
-            if index == 0:
-                counter1 += 1
-
+            if guess_param["wrong_position"][index] != "":
+                exclude_expression = exclude_letters + guess_param["wrong_position"][index]
             else:
-                counter1 += 1
-                if counter2 > 0:
-                    for i in range(counter2):
-                        correct_letters += guess_param["correct_positions"][position2+i]
-                    search_expression += f"{correct_letters}"
-                    counter2 = 0
-            if index == 4:
-                search_expression += f"{include_letters}{exclude_letters}{{{counter1}}}$"
+                exclude_expression = exclude_letters
+            if counter2 > 0:
+                for i in range(counter2):
+                    correct_letters += guess_param["correct_positions"][position2+i]
+                search_expression += f"{correct_letters}"
+                correct_letters = ""
+                counter2 = 0
+            search_expression += f"[^{exclude_expression}]{{1}}"
         else:
             if index == 0:
                 counter2 += 1
@@ -66,13 +65,11 @@ def generate_list(guess_param, word_list):
                 if counter2 == 0:
                     position2 = index
                 counter2 += 1
-                if counter1 > 0:
-                    search_expression += f"{include_letters}{exclude_letters}{{{counter1}}}"
-                    counter1 = 0
             if index == 4:
                 for i in range(counter2):
                     correct_letters += guess_param["correct_positions"][position2+i]
-                    search_expression += f"{correct_letters}$"
+                    search_expression += f"{correct_letters}"
+    search_expression += "$"
 
     #search_expression = f"{include_letters}^[^{guess_param['excluded_letters']}]{{5}}$"
     print(search_expression)
@@ -90,14 +87,13 @@ def update_parameters(guess_param, feedback):
         if result == "2":
             print("Correct letter and position")
             guess_param["correct_positions"][index] = letter
-            # if the letter is both correct and position remove from correct letter and move to position
-            if letter in guess_param["correct_letters"]:
-                guess_param["correct_letters"].remove(letter)
 
         elif result == "1":
             print("Correct letter but wrong position")
             if letter not in guess_param["correct_letters"] and letter not in guess_param["correct_positions"]:
                 guess_param["correct_letters"].append(letter)
+            if letter not in guess_param["wrong_position"][index]:
+                guess_param["wrong_position"][index] += letter
 
         else:
             print("Wrong letter")
@@ -111,13 +107,15 @@ feedback = 00000
 while feedback != 22222:
     print("Input 2 for correct letter and position, 1 for correct letter and wrong position, 0 for wrong letter: ")
     feedback = input(": ")
+    if feedback == "22222":
+        break
     update_parameters(guess_param, feedback)
     matching_words = generate_list(guess_param, matching_words)
     random_index = random.randint(0, len(matching_words) - 1)
     guess = matching_words[random_index]
     guess_param["guess"] = guess
     print(guess)
-
+print("Game Won!")
 
 
 
